011b7920556b55ba48808cb51fa446cfb638899d
SuicaDavid <genglanghuang@gmail.com>
2021-11-01 23:32:38 +0000

    init the project

---
+++ generator.ts
@@ -0,0 +1,7 @@
+import RomanNumeralGenerator from "./romanNumeralGenerator";
+
+class Generator implements RomanNumeralGenerator {
+    generate(number: number): string {
+        return ''
+    }
+}
---
+++ romanNumeralGenerator.ts
@@ -0,0 +1,3 @@
+export default interface RomanNumeralGenerator {
+   generate(number: number): string; 
+}
